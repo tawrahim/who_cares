398f77e578db26d1da3732e0be8371973ac17792
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-03-13 17:49:01 -0400

    Modified README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Who cares....?
+This is a dummy clone of twitter! Put my ruby knowledge to work.
+It is called who cares because honestly nobody really care about you life on twitter!
+
+## Philosophy
+* code with ruby
+* test with rspec
+* document with yardoc
+* host on heroku
+* If you repeat yourself more than three times, there is a better way to do it. refactor!
+* push code to github every night
+* Repeat
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhoCares::Application.config.secret_token = '15171fdc1610a1309340747126a3d426d795f65efad3daa6d84d87d5e08ac85431e6959a683d86701ff3559830c742ddb06dc1624ce80afa92b2b6489c1eafab'
