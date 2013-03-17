29a5ffddbc5618841002f4d24d4d00fdc2237104
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-03-17 13:34:43 -0400

    Created a helper for the page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   # Returns a title on a per page basis
   def full_title(page_title)
     base_title = 'Who cares?'
-    if base_title.empty?
+    if page_title.empty?
       base_title
     else
       "#{base_title} | #{page_title}"
