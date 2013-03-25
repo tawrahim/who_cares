9509c3d78ac8f1439e2f59274ecbbcd0b01b02f8
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-03-24 20:52:39 -0400

    Yard documetation stuffs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,23 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
+  # This is a hook method or sometimes refered to as callback
+  # methods, this gets called when an event is triggered
+  # before_save call back or hook would get executed before
+  # an active record object is saved
   before_save { |user| user.email = user.email.downcase }
+  before_save :hello
+  
+  # If you dont understand what call backs are, this method
+  # would demonstrate it to you, basically a hello message
+  # is printed before the active record object is saved
+  def hello
+    puts "Hello"
+  end
 
   # The validates method, presence makes
   # sure that the following fields are not left out blank
+  # These things are sort of like the table's properties
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+@\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
